42edb6524c32a3b9a778ff2f7469e36bbdf8b680
YN1CK <yannick.reiss@gmx.de>
2022-05-03 15:12:19 +0100

    init

---
+++ src/de/hsrm/cs/ads/Matrix.java
@@ -0,0 +1,14 @@
+package de.hsrm.cs.ads;
+
+public class Matrix {
+    double [][] data;
+    Matrix(int dimension){
+        //TODO
+    }
+
+    Matrix mult(Matrix that){
+        assert this.data.length == that.data.length;
+        //TODO
+        return null;
+    }
+}
